1ce2a7debe56f3b63efa082afb0d0899feba932c
habanerocity <lindyo87@gmail.com>
2022-08-02 20:48:06 -0700

    learned about manipulating html 5 data tags

--- js/script.js
+++ js/script.js
@@ -146,21 +146,86 @@ $(function () {
   // console.log(rangeInput.val());
 
   //simple slider
-  const galleryImage = $(".gallery").find("img").first();
-  const images = [
-    "images/laptop-mobile_small.jpg",
-    "images/laptop-on-table_small.jpg",
-    "images/people-office-group-team_small.jpg"
-  ];
+  // const galleryImage = $(".gallery").find("img").first();
+  // const images = [
+  //   "images/laptop-mobile_small.jpg",
+  //   "images/laptop-on-table_small.jpg",
+  //   "images/people-office-group-team_small.jpg"
+  // ];
 
-  let i = 0;
+  // let i = 0;
 
-  setInterval(function () {
-    i = (i + 1) % images.length;
-    galleryImage.fadeOut(function () {
-      $(this).attr("src", images[i]);
-      $(this).fadeIn();
-    });
-    console.log(galleryImage.attr("src"));
-  }, 2000)
+  // setInterval(function () {
+  //   i = (i + 1) % images.length;
+  //   galleryImage.fadeOut(function () {
+  //     $(this).attr("src", images[i]);
+  //     $(this).fadeIn();
+  //   });
+  //   console.log(galleryImage.attr("src"));
+  // }, 2000)
+
+  //jquery does not recognize !important
+
+  $(".gallery").css("display", "none");
+
+  // const redBox = $(".red-box");
+  // console.log(redBox.css("width"));
+  // console.log(redBox.width());
+
+  // redBox.css("background-color", "#AA7700");
+  // $("p").css("font-size", "18px");
+  // redBox.css("width", "+=20px");
+
+  // const properties = $("p").css(["font-size", "line-height", "color"]);
+  // console.log(properties);
+
+  // redBox.css("user-select", "none");
+
+  // $("a").addClass("fancy-link");
+  // $("p:first").addClass("large emphasize");
+
+  // $("li li").addClass(function (index) {
+  //   $("this").addClass("item-" + index);
+  // })
+
+  // $("div").addClass(function (index, currentClass) {
+  //   if (currentClass === "dummy") {
+  //     return "red-box";
+  //   }
+  // })
+
+  // $(".red-box").removeClass("red-box").addClass("blue-box");
+
+  // $(".dummy").removeClass("dummy").addClass("green-box");
+
+  // const gallery = $(".gallery");
+
+  // const images = [
+  //   "images/laptop-mobile_small.jpg",
+  //   "images/laptop-on-table_small.jpg",
+  //   "images/people-office-group-team_small.jpg"
+  // ];
+
+  // gallery.data("availableImages", images);
+  // console.log(gallery.data("availableImages"));
+
+  // gallery.data("name", "The Awesome Gallery");
+
+  // console.log(gallery.data());
+
+  // const firstPar = $("p:first");
+  // console.log(firstPar.data("mydata"));
+
+  //text(), html()
+  const firstPar = $("p:first");
+  console.log(firstPar.text());
+  console.log(firstPar.html());
+
+  firstPar.html("<strong>Hello</strong> world!");
+
+  //append some text to the first paragraph 
+
+  firstPar.html(firstPar.html() + "<p>this is some appended text</p>");
+
+
 });
